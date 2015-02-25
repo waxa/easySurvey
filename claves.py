352ba500bfc1e802f39ecc7f7ce72bd7d17c5577
waxa <waxa@gmail.com>
2015-02-25 12:42:10 +0100

    pos no

---
+++ claves.py
@@ -0,0 +1,2 @@
+headers = {"Authorization": "key=AIzaSyBSFHnJV7Q35CnsHcybhpQLSH6clyr    ltJE", "Content-Type" : "application/json", "Accept-Encoding" : "application    /json" }
+url = "https://android.googleapis.com/gcm/send"
